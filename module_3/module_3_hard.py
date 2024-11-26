250654b15014b41fc4bd5604af56600804a5b7c1
Anna <krasnyh.anna98@gmail.com>
2024-11-27 02:06:50 +0300

    version 2

--- module_3/module_3_hard.py
+++ module_3/module_3_hard.py
@@ -1,15 +1,25 @@
-def  calculate_structure_sum(*args):
-    print(*args)
-
-
+def  calculate_structure_sum(data_structure):
+    summ = 0
+    if isinstance(data_structure, int | float):
+        summ += data_structure
+    elif isinstance(data_structure, str):
+        summ += len(data_structure)
+    elif isinstance(data_structure, dict):
+        for key, value in data_structure.items():
+            summ = summ + len(key) + calculate_structure_sum(value)
+    elif isinstance(data_structure, list | tuple | set):
+        for j in data_structure:
+            summ += calculate_structure_sum(j)
+    return summ
 
 
 data_structure = [
-    [1, 2, 3],
-    {'a': 4, 'b': 5},
-    (6, {'cube': 7, 'drum': 8}),
-    "Hello",
-    ((), [{(2, 'Urban', ('Urban2', 35))}])
+    [1, 2, 3], # 6
+    {'a': 4, 'b': 5}, # 11
+    (6, {'cube': 7, 'drum': 8}), #29
+    "Hello", # 5
+    ((), [{(2, 'Urban', ('Urban2', 35))}]) #48
 ]
 
-calculate_structure_sum(data_structure)
+
+print(calculate_structure_sum(data_structure))
